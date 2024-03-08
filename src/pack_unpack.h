e9d84019b4589bad85cfb8c0159d9ed3c925ae27
MO <212637329@qq.com>
2024-03-08 16:17:18 +0800

    源码：增长其他用户的用户名字符串长度

--- src/pack_unpack.h
+++ src/pack_unpack.h
@@ -7,7 +7,7 @@ private:
 public:
     unsigned int cmdid; // 命令字
     char id[20];        // 包编号
-    char name[20];      // 发送者姓名
+    char name[50];      // 发送者姓名
     char hostname[20];  // 主机名
     char buf[1024];     // 附加信息
     cmd();
