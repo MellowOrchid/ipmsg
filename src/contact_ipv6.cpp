65c104cdcc6dc4cc17543b09e940e9448b6548eb
MO <212637329@qq.com>
2024-03-11 11:08:05 +0800

    源码：初始化双方地址

--- src/contact_ipv6.cpp
+++ src/contact_ipv6.cpp
@@ -1,24 +1,60 @@
 #include <iostream>
 #include "contact_ipv6.h"
-using std::cin, std::cout;
+#include "public.h"
+#include "write_log.h"
+using std::cin, std::cout, std::cerr, std::string;
 
 contact_ipv6::contact_ipv6() {}
 
 contact_ipv6::~contact_ipv6() {}
 
+/** 初始化本机信息 */
 void contact_ipv6::init()
 {
-    // 初始化本机信息
     cout << "请输入共有 IPv6 地址：";
     cin >> myip6;
     cout << "请输入目标 IPv6 地址：";
     cin >> dest_ip6;
-    
+
+    // 创建 UDP 套接字
+    udp_sock6 = socket(AF_INET6, SOCK_DGRAM, 0);
+    if (udp_sock6 < 0)
+    {
+        lmsg = "创建 IPv6 UDP 套接字失败";
+        wlog::log(lmsg);
+        cerr << lmsg << '\n';
+        return;
+    }
+
+    // 创建 TCP 套接字
+    tcp_sock6 = socket(AF_INET6, SOCK_STREAM, 0);
+    if (tcp_sock6 < 0)
+    {
+        lmsg = "创建 IPv6 TCP 套接字失败";
+        wlog::log(lmsg);
+        cerr << lmsg << '\n';
+        return;
+    }
+
+    memset(&udp_sock_addr6, 0, sizeof(udp_sock_addr6));
+    udp_sock_addr6.sin6_family = AF_INET6;
+    udp_sock_addr6.sin6_port = htons(MSG_PORT);
+    inet_pton(AF_INET6, myip6, &udp_sock_addr6.sin6_addr);
+
+    memset(&tcp_sock_addr6, 0, sizeof(tcp_sock_addr6));
+    tcp_sock_addr6.sin6_family = AF_INET6;
+    tcp_sock_addr6.sin6_port = htons(MSG_PORT);
+    inet_pton(AF_INET6, myip6, &tcp_sock_addr6.sin6_addr);
+
+    lmsg = "IPv6 套接字创建成功";
+    wlog::log(lmsg);
+    cout << lmsg << '\n';
 }
 
+/** 与目标用户建立联系 */
 void contact_ipv6::contact()
 {
-    // 与目标用户交流
+    init();
 }
 
 void contact_ipv6::send_message()
--- src/contact_ipv6.h
+++ src/contact_ipv6.h
@@ -4,15 +4,15 @@
 class contact_ipv6
 {
 private:
-    int udp_sock6;
-    int tcp_sock6;
-    sockaddr_in6 udp_sock_addr6;
-    sockaddr_in6 tcp_sock_addr6;
-    char myip6[45];
-    char dest_ip6[45];
-    void init();
-    void send_message();
-    void send_file();
+    static int udp_sock6;
+    static int tcp_sock6;
+    static sockaddr_in6 udp_sock_addr6;
+    static sockaddr_in6 tcp_sock_addr6;
+    static char myip6[45];
+    static char dest_ip6[45];
+    static void init();
+    static void send_message();
+    static void send_file();
 
 public:
     contact_ipv6();
